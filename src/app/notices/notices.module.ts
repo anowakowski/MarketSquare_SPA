6ce4e939f7b364f76be3580b7d1a6a4e3e2897d1
anowakowski <anowakowski@mailc.net>
2020-02-13 12:03:31 +0100

    add notices module

---
+++ src/app/notices/notices.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NoticesComponent } from './notices.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [NoticesComponent]
+})
+export class NoticesModule { }
