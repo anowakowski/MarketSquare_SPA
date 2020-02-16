d260bba3af4cd8f7363390e761262cc2408b258a
anowakowski <anowakowski@mailc.net>
2020-02-16 21:24:52 +0100

    added user with services

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  getAllUsers() {
+    this.httpClient.get<any>('http://localhost:5000/api/' + 'users/getAllUsers').toPromise().then(response => response);
+  }
 }
