e67ab3252d92464362005286d97611ae6853e334
anowakowski <anowakowski@mailc.net>
2020-02-17 21:21:18 +0100

    added new tags with a materials

--- src/app/modules/notices/components/add-new-notices/add-new-notices.component.ts
+++ src/app/modules/notices/components/add-new-notices/add-new-notices.component.ts
@@ -1,12 +1,28 @@
 import { Component, OnInit } from "@angular/core";
 import { FormGroup, FormControl, Validators } from "@angular/forms";
+import {COMMA, ENTER} from '@angular/cdk/keycodes';
+import { MatChipInputEvent } from '@angular/material/chips';
 
+export interface Fruit {
+  name: string;
+}
 @Component({
   selector: "app-add-new-notices",
   templateUrl: "./add-new-notices.component.html",
   styleUrls: ["./add-new-notices.component.css"]
 })
 export class AddNewNoticesComponent implements OnInit {
+
+  visible = true;
+  selectable = true;
+  removable = true;
+  addOnBlur = true;
+  readonly separatorKeysCodes: number[] = [ENTER, COMMA];
+  fruits: Fruit[] = [
+    {name: 'Lemon'},
+    {name: 'Lime'},
+    {name: 'Apple'},
+  ];
 
   public addNewNoticesForm = new FormGroup({
     name: new FormControl("", Validators.required),
@@ -19,5 +35,29 @@ export class AddNewNoticesComponent implements OnInit {
 
   onSubmit() {
     alert(JSON.stringify(this.addNewNoticesForm.value));
+  }
+
+
+  add(event: MatChipInputEvent): void {
+    const input = event.input;
+    const value = event.value;
+
+    // Add our fruit
+    if ((value || '').trim()) {
+      this.fruits.push({name: value.trim()});
+    }
+
+    // Reset the input value
+    if (input) {
+      input.value = '';
+    }
+  }
+
+  remove(fruit: Fruit): void {
+    const index = this.fruits.indexOf(fruit);
+
+    if (index >= 0) {
+      this.fruits.splice(index, 1);
+    }
   }
 }
