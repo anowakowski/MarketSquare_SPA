a4c827a3daf7d3165562fde8ca3b67cd9c62957c
Wojciech Doroszkiewicz <doroszkiewicz666@gmail.com>
2020-02-17 10:49:17 +0100

    Create Notices list component

--- src/app/notices/notices.module.ts
+++ src/app/notices/notices.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NoticesComponent } from './notices.component';
 import { NoticesRoutingModule } from './notices-routing.module';
+import { AllNoticesListComponent } from './components/all-notices-list/all-notices-list.component';
 
 @NgModule({
   imports: [
     CommonModule,
     NoticesRoutingModule
   ],
-  declarations: [NoticesComponent]
+  declarations: [NoticesComponent, AllNoticesListComponent]
 })
 export class NoticesModule { }
