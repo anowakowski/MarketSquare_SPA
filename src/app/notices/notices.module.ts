083b4a48977b064436d7d5b7902b739ae794da18
anowakowski <anowakowski@mailc.net>
2020-02-17 10:56:30 +0100

    added new add new component

--- src/app/notices/notices.module.ts
+++ src/app/notices/notices.module.ts
@@ -3,12 +3,17 @@ import { CommonModule } from '@angular/common';
 import { NoticesComponent } from './notices.component';
 import { NoticesRoutingModule } from './notices-routing.module';
 import { AllNoticesListComponent } from './components/all-notices-list/all-notices-list.component';
+import { AddNewNoticesComponent } from './components/add-new-notices/add-new-notices.component';
 
 @NgModule({
   imports: [
     CommonModule,
     NoticesRoutingModule
   ],
-  declarations: [NoticesComponent, AllNoticesListComponent]
+  declarations: [
+    NoticesComponent,
+    AllNoticesListComponent,
+    AddNewNoticesComponent
+  ]
 })
 export class NoticesModule { }
