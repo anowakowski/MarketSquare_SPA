be3797a980b1c5076ef1ab26a0de104163ee4e1a
anowakowski <anowakowski@mailc.net>
2020-02-17 13:09:44 +0100

    fixed services

--- src/app/modules/notices/notices.component.ts
+++ src/app/modules/notices/notices.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { UserService } from "src/app/services/user.service";
 
 @Component({
-  selector: 'app-notices',
-  templateUrl: './notices.component.html',
-  styleUrls: ['./notices.component.css']
+  selector: "app-notices",
+  templateUrl: "./notices.component.html",
+  styleUrls: ["./notices.component.css"]
 })
 export class NoticesComponent implements OnInit {
+  constructor(private userService: UserService) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  getAllUsers() {
+    this.userService.getAllUsers().then(response => {
+      const users = response;
+    });
   }
-
 }
