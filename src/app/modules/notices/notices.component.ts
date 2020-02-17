d1e5147403b62dce22486aeb84ddeab73fa2d01e
Wojciech Doroszkiewicz <doroszkiewicz666@gmail.com>
2020-02-17 14:54:18 +0100

    AddNoticeService

--- src/app/modules/notices/notices.component.ts
+++ src/app/modules/notices/notices.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { UserService } from "src/app/services/user.service";
+import { NoticeService } from "src/app/services/notice.service";
 
 @Component({
   selector: "app-notices",
@@ -7,13 +8,19 @@ import { UserService } from "src/app/services/user.service";
   styleUrls: ["./notices.component.css"]
 })
 export class NoticesComponent implements OnInit {
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService, private noticeService: NoticeService) {}
 
   ngOnInit() {}
 
   getAllUsers() {
     this.userService.getAllUsers().then(response => {
       const users = response;
+    });
+  }
+  getAllNotices() {
+    this.noticeService.getAllNotices().then(response => {
+      const notices = response;
+      console.log(notices);
     });
   }
 }
