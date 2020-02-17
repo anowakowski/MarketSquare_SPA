5644ef2a206bbd8a8e51b32b29db14677c7c63f3
KamilMarkuszewski <kamilmarkuszewski89@gmail.com>
2020-02-17 15:37:29 +0100

    revert changes

--- src/app/modules/notices/components/add-new-notices/add-new-notices.component.ts
+++ src/app/modules/notices/components/add-new-notices/add-new-notices.component.ts
@@ -1,23 +1,15 @@
-import { Component, OnInit } from "@angular/core";
-import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: "app-add-new-notices",
-  templateUrl: "./add-new-notices.component.html",
-  styleUrls: ["./add-new-notices.component.css"]
+  selector: 'app-add-new-notices',
+  templateUrl: './add-new-notices.component.html',
+  styleUrls: ['./add-new-notices.component.css']
 })
 export class AddNewNoticesComponent implements OnInit {
 
-  public addNewNoticesForm = new FormGroup({
-    name: new FormControl("", Validators.required),
-    description: new FormControl("", Validators.required)
-  });
+  constructor() { }
 
-  constructor() {}
-
-  ngOnInit() {}
-
-  onSubmit() {
-    alert(JSON.stringify(this.addNewNoticesForm.value));
+  ngOnInit() {
   }
+
 }
