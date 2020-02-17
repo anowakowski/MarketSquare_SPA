5edb3a677a5fd9f9128331508b08b5b0125d540c
KamilMarkuszewski <kamilmarkuszewski89@gmail.com>
2020-02-17 15:42:58 +0100

    merge into my branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
@@ -14,6 +15,7 @@ import { UserService } from './services/user.service';
   imports: [
     HttpClientModule,
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [UserService],
--- src/app/modules/notices/notices.module.ts
+++ src/app/modules/notices/notices.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { NoticesComponent } from './notices.component';
 import { NoticesRoutingModule } from './notices-routing.module';
 import { AllNoticesListComponent } from './components/all-notices-list/all-notices-list.component';
@@ -10,6 +12,8 @@ import { TagListComponent } from './components/tag-list/tag-list.component';
 
 @NgModule({
   imports: [
+    FormsModule,
+    ReactiveFormsModule,
     CommonModule,
     NoticesRoutingModule
   ],
