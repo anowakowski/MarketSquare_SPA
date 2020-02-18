fa14fefb12b928e2f58cf9828a871e353e3c0122
KamilMarkuszewski <kamilmarkuszewski89@gmail.com>
2020-02-18 09:49:28 +0100

    undo local change

--- src/app/modules/notices/components/all-notices-list/all-notices-list.component.ts
+++ src/app/modules/notices/components/all-notices-list/all-notices-list.component.ts
@@ -24,32 +24,32 @@ export class AllNoticesListComponent implements OnInit {
     });
   }
   fillNotices() {
-    // this.notices = [
-    //   {
-    //     id: 1,
-    //     name: "I will buy a bunch of honey",
-    //     description: "I need a lof of nice honey to produce alkohol",
-    //     creatorName: "Justyna Kowalska",
-    //     creationDate: new Date("1/16/2020"),
-    //     tags: ["food", "honey", "buy"]
-    //   },
-    //   {
-    //     id: 2,
-    //     name: "Lot of good honey for sell",
-    //     description: "I have honey from my grandpartes, they live in small village",
-    //     creatorName: "Adam Nowak",
-    //     creationDate: new Date("10/19/2019"),
-    //     tags: ["food", "honey", "sell"]
-    //   },
-    //   {
-    //     id: 3,
-    //     name: "Old school bike for sell",
-    //     description: "Very pretty and well painted bike",
-    //     creatorName: "Jan Jarząbek",
-    //     creationDate: new Date("1/23/2020"),
-    //     tags: ["bicycle", "sell"]
-    //   }
-    // ];
+    this.notices = [
+      {
+        id: 1,
+        name: "I will buy a bunch of honey",
+        description: "I need a lof of nice honey to produce alkohol",
+        creatorName: "Justyna Kowalska",
+        creationDate: new Date("1/16/2020"),
+        tags: ["food", "honey", "buy"]
+      },
+      {
+        id: 2,
+        name: "Lot of good honey for sell",
+        description: "I have honey from my grandpartes, they live in small village",
+        creatorName: "Adam Nowak",
+        creationDate: new Date("10/19/2019"),
+        tags: ["food", "honey", "sell"]
+      },
+      {
+        id: 3,
+        name: "Old school bike for sell",
+        description: "Very pretty and well painted bike",
+        creatorName: "Jan Jarząbek",
+        creationDate: new Date("1/23/2020"),
+        tags: ["bicycle", "sell"]
+      }
+    ];
   }
   formatDate(date) {
   const monthNames = [
