f82a768edd91ec7a1711f42baa29363b79ba319f
KamilMarkuszewski <kamilmarkuszewski89@gmail.com>
2020-02-18 09:35:10 +0100

    changes on settings tab

--- src/app/modules/settings/components/tag-subscriptions/tag-subscriptions.component.ts
+++ src/app/modules/settings/components/tag-subscriptions/tag-subscriptions.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { SettingsService } from '../../services/settings.service';
 
 @Component({
   selector: 'app-tag-subscriptions',
@@ -7,9 +8,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TagSubscriptionsComponent implements OnInit {
 
-  constructor() { }
+  subscribedTags: string[];
+  blacklistedTags: string[];
+
+  @ViewChild('subscribeNameInput', {static: false}) subscribeNameInput: ElementRef;
+  @ViewChild('blacklistNameInput', {static: false}) blacklistNameInput: ElementRef;
+
+  constructor(private settingsService: SettingsService) { }
 
   ngOnInit() {
+    this.refresh();
+  }
+
+  refresh() {
+    this.getBlacklistedTags();
+    this.getSubscribedTags();
+  }
+
+  subscribeTag() {
+    const subscribedTag = this.subscribeNameInput.nativeElement.value;
+    this.settingsService.subscribeTag(subscribedTag).then(response => {
+      this.refresh();
+    });
+  }
+
+  blacklistTag() {
+    const subscribedTag = this.blacklistNameInput.nativeElement.value;
+    this.settingsService.blacklistTag(subscribedTag).then(response => {
+      this.refresh();
+    });
+  }
+
+  unsubscribeTag(tag: string) {
+    this.settingsService.unsubscribeTag(tag).then(response => {
+      this.refresh();
+    });
+  }
+
+  unblacklistTag(tag: string) {
+    this.settingsService.unblacklistTag(tag).then(response => {
+      this.refresh();
+    });
+  }
+
+  getSubscribedTags() {
+    this.settingsService.getSubscribedTags().then(response => {
+      this.subscribedTags = response;
+    });
+  }
+
+  getBlacklistedTags() {
+    this.settingsService.getBlacklistedTags().then(response => {
+      this.blacklistedTags = response;
+    });
   }
 
 }
--- src/app/modules/settings/components/user-subscriptions/user-subscriptions.component.ts
+++ src/app/modules/settings/components/user-subscriptions/user-subscriptions.component.ts
@@ -8,17 +8,47 @@ import { SettingsService } from '../../services/settings.service';
   styleUrls: ['./user-subscriptions.component.css']
 })
 export class UserSubscriptionsComponent implements OnInit {
+  subscribedUsers: string[];
+  blacklistedUsers: string[];
 
-  @ViewChild('nameInput', {static: false}) nameInput: ElementRef;
+  @ViewChild('subscribeNameInput', {static: false}) subscribeNameInput: ElementRef;
+  @ViewChild('blacklistNameInput', {static: false}) blacklistNameInput: ElementRef;
 
   constructor(private settingsService: SettingsService) { }
 
   ngOnInit() {
+    this.getBlacklistedUsers();
+    this.getSubscribedUsers();
   }
 
   subscribeUser() {
-    const subscribedUser = this.nameInput.nativeElement.value;
+    const subscribedUser = this.subscribeNameInput.nativeElement.value;
     this.settingsService.subscribeUser(subscribedUser);
+  }
+
+  blacklistUser() {
+    const subscribedUser = this.blacklistNameInput.nativeElement.value;
+    this.settingsService.blacklistUser(subscribedUser);
+  }
+
+  unsubscribeUser(username: string) {
+    this.settingsService.unsubscribeUser(username);
+  }
+
+  unblacklistUser(username: string) {
+    this.settingsService.unblacklistUser(username);
+  }
+
+  getSubscribedUsers() {
+    this.settingsService.getSubscribedUsers().then(response => {
+      this.subscribedUsers = response;
+    });
+  }
+
+  getBlacklistedUsers() {
+    this.settingsService.getBlacklistedUsers().then(response => {
+      this.blacklistedUsers = response;
+    });
   }
 
 }
