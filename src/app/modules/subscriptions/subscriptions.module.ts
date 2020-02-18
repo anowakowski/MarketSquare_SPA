9fefa08af3bdf35021269fa87e197da5b949931d
anowakowski <anowakowski@mailc.net>
2020-02-18 15:27:50 +0100

    added breadcamp

--- src/app/modules/subscriptions/subscriptions.module.ts
+++ src/app/modules/subscriptions/subscriptions.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SubscriptionsComponent } from './subscriptions.component';
 import { SubscriptionsRoutingModule } from './subscriptions-routing.module';
+import { SubscriptionsListComponent } from './components/subscriptions-list/subscriptions-list.component';
 
 @NgModule({
   imports: [
     CommonModule,
     SubscriptionsRoutingModule
   ],
-  declarations: [SubscriptionsComponent]
+  declarations: [SubscriptionsComponent, SubscriptionsListComponent]
 })
 export class SubscriptionsModule { }
