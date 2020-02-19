7a56c02b7ac50cff7ffd28615b57ad9fe261965b
anowakowski <anowakowski@mailc.net>
2020-02-19 19:52:09 +0100

    added firebase configs files

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,61 @@
 import { Injectable } from '@angular/core';
 
+import { auth } from 'firebase/app';
+import { AngularFireAuth } from '@angular/fire/auth';
+
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
+
+import { Observable, of, merge } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+
+import { Router } from '@angular/router';
+import { User } from '../models/user';
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  user$: Observable<any>;
+
+  constructor(
+    private afAuth: AngularFireAuth,
+    private afs: AngularFirestore,
+    private router: Router
+    ) {
+      this.user$ = afAuth.authState.pipe(
+        switchMap(user => {
+          if (user) {
+            return this.afs.doc<User>(`users/${user.uid}`).valueChanges();
+          } else {
+            return of(null);
+          }
+        })
+      );
+  }
+
+  async googleSignin() {
+    const provider = new auth.GoogleAuthProvider();
+    const credential = await this.afAuth.auth.signInWithPopup(provider);
+    return this.updateUserData(credential.user);
+  }
+
+  updateUserData(user: firebase.User) {
+    const userRef: AngularFirestoreDocument<User> = this.afs.doc(`users/${user.uid}`);
+
+    const data = {
+      uid: user.uid,
+      email: user.email,
+      splayName: user.displayName,
+      photoURL: user.photoURL
+    };
+
+    return userRef.set(data, {merge: true});
+  }
+
+  signOut() {
+    this.afAuth.auth.signOut().then(() => {
+      this.router.navigate(['/']);
+    });
+  }
 }
